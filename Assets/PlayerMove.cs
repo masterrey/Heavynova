aa2f1950055220bca3114fc99caee595653432d8
Reinaldo Ramos <masterrey@gmail.com>
2022-04-01 22:58:47 -0300

    robo

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public CharacterController characterController;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(move);
+    }
+}
